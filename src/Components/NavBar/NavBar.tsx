800e231295a0e35778274eb0fd3a0285e09c94f3
Tonyaap <stefan_kniest_@hotmail.com>
2020-10-29 12:30:56 +0100

    show login or signin based on presence of token

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import {logOut} from "../../store/user/actions"
+import {selectToken} from "../../store/user/selectors"
+
+
 
 export default function NavBar() {
 
   const dispatch = useDispatch();
+  const token = useSelector(selectToken)
 
   return (
     <div className="topnav">
@@ -26,19 +30,22 @@ export default function NavBar() {
             Reflect
           </NavLink>
         </li>
-        <li>
-          <NavLink style={{ color: "white" }} to="/SignUp">
-            Sing up
-          </NavLink>
-        </li>
-        <li>
+
+    
+        { !token ?      <li>
           <NavLink style={{ color: "white" }} to="/Login">
             Login
           </NavLink>
-        </li>
-          <li> 
+        </li>  :   <li> 
           <button onClick={() => dispatch(logOut())}> logout </button>
-           </li>
+           </li>  }
+        <li>
+          <NavLink style={{ color: "white" }} to="/SignUp">
+            Sign up
+          </NavLink>
+        </li>
+   
+        
       </ul>
     </div>
   );
