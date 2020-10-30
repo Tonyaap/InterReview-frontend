92eec1c83e0fd2b1dd89526fe50ddb25bc50125c
Tonyaap <stefan_kniest_@hotmail.com>
2020-10-30 10:22:14 +0100

    landing page

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,10 +4,28 @@ import Logo from "../logo/logo.png"
 export default function HomePage() {
   return (
     <div>
-      <p>hello user this is homepage</p>
+     <br></br>
+       <br></br>
+         <br></br>
+           <br></br>
+       
       <img src={Logo}/>
-
-
+       <br></br>
+          <br></br>
+             <br></br>
+                
+      <div className="About
+      "> InterReview is a tool to self assess your job interviews.
+            This App allows a user to keep track of the companies you talked to.
+            By scoring yourself each interview you will see improvement over time. </div>
+  <br></br>
+           <br></br>
+           
+           <div className="pleaselogin">
+           <p> To get started please <a href="/login">Login</a> or  <a href="/signup">Sign-Up</a> </p>
+           </div>
     </div>
+        
+
   );
 }
